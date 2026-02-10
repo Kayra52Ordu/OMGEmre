e81bdd4e637971320be74d20b1a3336edf472e06
TechnikFox <42148171+TechnikFox66@users.noreply.github.com>
2026-02-10 12:46:27 +0100

    Abonnieren Kommentar

--- OMGEmre/Program.cs
+++ OMGEmre/Program.cs
@@ -17,6 +17,7 @@ namespace OMGEmre
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Abonnieren sie TechnikFox
         }
     }
 }
